9b9ba337c97a1cc8d90e0e0edb2b3f6647d91c48
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-07-27 22:24:57 +0900

    add super resolution test

--- super_resolution_test.py
+++ super_resolution_test.py
@@ -2,51 +2,32 @@
 
 import glob
 import numpy as np
-import convert_network
-import train_network
 from tensorflow.python.keras.models import load_model, model_from_json
-from tensorflow.python.keras.layers import Input
 from tensorflow.python.keras.preprocessing.image import (
     load_img, img_to_array, array_to_img)
-from tensorflow.python.keras.applications.vgg16 import VGG16
 
 
 # テスト
-def test(covert_model, images_list, input_shape=(224, 224, 3)):
-    # 変換
-    predict = covert_model.predict(images_list)
+def test(model, images_list, input_shape=(224, 224, 3)):
+    predict = model.predict(images_list)
     for i in range(predict.shape[0]):
         # 保存できる画像に変換
         predict_image = array_to_img(predict[i])
         # 保存
-        file_name = f'{test_files}/predicted_images/test{i}.jpg'
+        file_name = f'{test_files}predicted_images/test{i}.jpg'
         predict_image.save(file_name)
         print(f'>> predict! test{i}.jpg')
-
-
-# コンテンツ入力画像のパスをすべて取得
-def get_img_path_list(path):
-    img_path_list = glob.glob(path)
-    return img_path_list
 
 
 # ワイルドカードからファイルを取得する
 def get_path_using_glob(path):
     return glob.glob(path)
 
-"""
-def get_vgg16_layer_dict():
-    layers = {}
-    vgg16 = VGG16()
-    # 学習させない設定をする
-    print(vgg16.layers[0])
-    for layer in vgg16.layers:
-        layer.trainable = False
-        print(layer)
-        # layers[layer['name']] = layer
-    print(layers)
-    return layers
-"""
+
+# コンテンツ入力画像のパスをすべて取得
+def get_img_path_list(path):
+    img_path_list = glob.glob(path)
+    return img_path_list
 
 
 # 画像パスリストから画像データ配列を得る
@@ -58,27 +39,18 @@ def get_images_array_from_path_list(img_path_list, image_size=(224, 224)):
 
 
 if __name__ == '__main__':
-    test_files = 'production/style_transfer/'
-    # model_json_path = test_files + '*.json'
+    test_files = 'production/super_resolution/'
+    model_json_path = test_files + '*.json'
     weight_loss_path = test_files + '*.h5'
     test_images_path = test_files + 'test_images/*.jpg'
 
-    # model_json_path = get_path_using_glob(model_json_path)
+    model_json_path = get_path_using_glob(model_json_path)
     weight_loss_path = get_path_using_glob(weight_loss_path)
     test_images_path = get_path_using_glob(test_images_path)
 
-    # 変換ネットワーク
-    convert_model = convert_network.build_network()
-    # モデル構築
-    network = train_network.TrainNet()
-    model = network.rebuild_vgg16(convert_model.output,
-                                  True, True, convert_model.input)
-
     # モデル読み込み
-    # model_json_path = open(model_json_path[0]).read()
-    # input_data = Input(shape=(1, 224, 224, 3))
-    # model = model_from_json(model_json_path, get_vgg16_layer_dict())
-    # model = model_from_json(model_json_path)
+    model_json_path = open(model_json_path[0]).read()
+    model = model_from_json(model_json_path)
 
     print('>> load model.')
     # 重み読み込み
@@ -89,5 +61,5 @@ if __name__ == '__main__':
     print('>> get image path.')
     # 変換
     print('>> test start.')
-    test(convert_model, images_list)
+    test(model, images_list)
     print('>> test finish.')
